27870722467e74a73ed66e8e4f6a42b37328ff5f
Eklezyarez <eklezyarez@gmail.com>
2022-08-10 23:12:23 -0300

    update

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente{
+    agencia;
+    saldo;
+}
 
+const cliente1 = new Cliente();
 cliente1.nome = "Joaquim";
 cliente1.cpf = 11122233344;
 cliente1.rg = 112223334
-cliente1.agencia = 1000;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Joana";
 cliente2.cpf = 44455566677;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1000;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+let valorSacado = 200;
+contaCorrenteRicardo.saldo -= valorSacado;
+
+
+
+
+console.log(cliente1, cliente1);
 console.log(cliente1, cliente2);
